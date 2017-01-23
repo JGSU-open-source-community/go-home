af63670078cb870818d9d79dc7a3ecf4801ff0e5
liyu4 <yangxiaoxiu@live.cn>
2017-01-23 14:23:37 +0800

    Add imitate data

--- call/journey_test.go
+++ call/journey_test.go
@@ -1,11 +1,103 @@
 package call
 
 import (
+	"encoding/json"
+	"github.com/olekukonko/tablewriter"
+	"io/ioutil"
+	"os"
 	"testing"
 )
 
-func TestCall(t *testing.T) {
-	datas := Call("G4775")
+// func TestCall(t *testing.T) {
+// 	datas := Call("G4775")
 
-	t.Log(string(datas))
+// 	t.Log(string(datas))
+// }
+
+func TestShowLeftTricket(t *testing.T) {
+	table := tablewriter.NewWriter(os.Stdout)
+
+	f, err := ioutil.ReadFile("./testlefttricketdata.json")
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	var v interface{}
+
+	if err := json.Unmarshal(f, &v); err != nil {
+		t.Fatal(err)
+	}
+
+	table.SetHeader([]string{"车次", "出发站", "到达站", "出发时间", "到达时间", "商务座", "特等座", "一等座", "二等座", "高级软卧", "软卧", "硬卧", "软座", "硬座", "无座", "其他"})
+	if m, ok := v.(map[string]interface{}); ok {
+		if m["httpstatus"].(float64) == 200 {
+			if data, ok := m["data"].([]interface{}); ok {
+				for _, queryLeftNewDTO := range data {
+					if ql, ok := queryLeftNewDTO.(map[string]interface{}); ok {
+						raw := ql["queryLeftNewDTO"]
+						detail := raw.(map[string]interface{})
+
+						// 车次
+						station_train_code := detail["station_train_code"].(string)
+						// 出发站
+						from_station_name := detail["from_station_name"].(string)
+						// 到达站
+						to_station_name := detail["to_station_name"].(string)
+						// 出发时间
+						satrt_time := detail["start_time"].(string)
+						// 到达时间
+						arrive_time := detail["arrive_time"].(string)
+						// 商务座
+						swz_nun := detail["swz_num"].(string)
+						// 特等座
+						tz_num := detail["tz_num"].(string)
+						// 一等座
+						zy_num := detail["zy_num"].(string)
+						// 二等座
+						ze_num := detail["ze_num"].(string)
+						// 高级软卧
+						gr_num := detail["gr_num"].(string)
+						// 软卧
+						rw_num := detail["rw_num"].(string)
+						// 硬卧
+						yw_num := detail["yw_num"].(string)
+						// 软座
+						rz_num := detail["rz_num"].(string)
+						// 硬座
+						yz_num := detail["yz_num"].(string)
+						// 无座
+						wz_num := detail["wz_num"].(string)
+						// 其他
+						qt_num := detail["qt_num"].(string)
+
+						row := []string{
+							station_train_code,
+							from_station_name,
+							to_station_name,
+							satrt_time,
+							arrive_time,
+							swz_nun,
+							tz_num,
+							zy_num,
+							ze_num,
+							gr_num,
+							rw_num,
+							yw_num,
+							rz_num,
+							yz_num,
+							wz_num,
+							qt_num,
+						}
+						table.Append(row)
+					}
+				}
+			}
+		} else {
+			t.Fatal("invalid left tricket message!")
+			return
+		}
+	}
+	table.Render()
+
 }
