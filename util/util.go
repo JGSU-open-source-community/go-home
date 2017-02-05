d5152a486266c2dba59a5eccb7ad3bd8f25feb63
liyu4 <yangxiaoxiu@live.cn>
2017-02-05 20:45:50 +0800

    add update argument

--- util/util.go
+++ util/util.go
@@ -1,8 +1,6 @@
 package util
 
 import (
-	"encoding/json"
-	"io/ioutil"
 	"regexp"
 	"strings"
 	"time"
@@ -24,69 +22,6 @@ func Stations(stationTetx []byte) map[string]string {
 		cityMap2Code[temp[0]] = temp[1]
 	}
 	return cityMap2Code
-}
-
-type Station struct {
-	Train_no string
-	From     string
-	To       string
-}
-
-var All = make(map[string]map[string]*Station)
-
-// Update
-func TrainList() error {
-	bytes, err := ioutil.ReadFile("trainlist.json")
-
-	if err != nil {
-		return err
-	}
-
-	var v interface{}
-	if err := json.Unmarshal(bytes, &v); err != nil {
-		return err
-	}
-
-	if m, ok := v.(map[string]interface{}); ok {
-		for k, endDate := range m {
-			if endDate == nil {
-				continue
-			}
-
-			var oneday = make(map[string]*Station)
-
-			for _, trainType := range endDate.(map[string]interface{}) {
-				for _, trains := range trainType.([]interface{}) {
-					obj := trains.(map[string]interface{})
-					stc := strings.Split(obj["station_train_code"].(string), "(")
-					cityTocity := strings.TrimSuffix(stc[1], ")")
-
-					combain := strings.Split(cityTocity, "-")
-
-					there := combain[0]
-					home := combain[1]
-
-					oneday[stc[0]] = &Station{
-						Train_no: obj["train_no"].(string),
-						From:     there,
-						To:       home,
-					}
-				}
-			}
-
-			All[k] = oneday
-		}
-	}
-
-	marshl, err := json.Marshal(All)
-
-	if err != nil {
-		return err
-	}
-
-	ioutil.WriteFile("compress.data", marshl, 0644)
-
-	return nil
 }
 
 func compare(t1, t2 string) bool {
