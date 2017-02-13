3503d66c163bf12ce2e94fd0c940c7cb5923ecd3
liyu4 <yangxiaoxiu@live.cn>
2017-02-13 18:30:04 +0800

    computer distance between A and B by longitude and latitude

--- util.go
+++ util.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"math"
 	"regexp"
 	"strings"
 	"time"
@@ -32,4 +33,20 @@ func compare(t1, t2 string) bool {
 		return true
 	}
 	return false
+}
+
+// 根据经纬度计算两地距离
+func earthDistance(lat1, lng1, lat2, lng2 float64) float64 {
+	radius := 6371000 // 6378137
+	rad := math.Pi / 180.0
+
+	lat1 = lat1 * rad
+	lng1 = lng1 * rad
+	lat2 = lat2 * rad
+	lng2 = lng2 * rad
+
+	theta := lng2 - lng1
+	dist := math.Acos(math.Sin(lat1)*math.Sin(lat2) + math.Cos(lat1)*math.Cos(lat2)*math.Cos(theta))
+
+	return dist * radius
 }
