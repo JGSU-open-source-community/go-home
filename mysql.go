faf2fed4eef36a33dac0a5d80f6975030a49244c
liyu4 <yangxiaoxiu@live.cn>
2017-02-24 20:59:05 +0800

    change file name from toMysql.go to mysql for explict expression

---
+++ mysql.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"encoding/csv"
+	"log"
+	"os"
+	"strings"
+
+	"github.com/astaxie/beego/orm"
+)
+
+// func writeRawDataToMysql() {
+// 	staions := allChinaRailwayStations(stationName())
+// 	data := []byte(strings.Join(staions, "站\n"))
+// 	ioutil.WriteFile("stations.txt", data, 0644)
+// }
+
+const (
+	station_lat_lgt = "station_lat_lgt"
+)
+
+func latitudeAndLongitude() {
+	// Load a csv file
+	f, err := os.Open("lt.csv")
+	if err != nil {
+		log.Fatal(err)
+		os.Exit(2)
+	}
+
+	defer f.Close()
+
+	// Create a new reader
+	r := csv.NewReader(f)
+
+	// A successful call returns err == nil this is different with r.Read()
+	records, err := r.ReadAll()
+	if err != nil {
+		log.Fatal(err)
+		os.Exit(2)
+	}
+
+	for index, singleRecord := range records {
+		if index == 0 {
+			continue
+		}
+
+		sql := `insert into  ` + station_lat_lgt + ` (station, latitude, longitude)` +
+			`values('` + strings.TrimSuffix(singleRecord[8], "站") + `', '` + singleRecord[2] + `', '` + singleRecord[3] + `')
+			`
+		if err := insert(sql); err != nil {
+			log.Println(err)
+		}
+	}
+}
+
+func insert(sql string) error {
+	o := orm.NewOrm()
+	if _, err := o.Raw(sql).Exec(); err != nil {
+		return err
+	}
+	return nil
+}
